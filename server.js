005e10ef19deb649ee35fd92f6fc614064cfa8dd
Aasavaliya <157261669+Ashish13590@users.noreply.github.com>
2024-04-12 06:45:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -13,25 +13,24 @@
 
 
 const legoData = require("./modules/legoSets");
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('public'));
 
-// Initialize Lego Data
-legoData.initialize()
-    .then(() => {
+
         // Routes
         app.get("/", (req, res) => {
-            // <div class="w-[150px] h-[80px] shadow-2xl bg-white rounded-lg flex justify-center items-center">
-            //     <p class="text-center">shadow-2xl</p>
-            // </div>
-        res.sendFile(path.join(__dirname,"views/index.html"));
-           
-                    //res.json("Assignment 2: Your Name - Your Student ID");
+            res.sendFile(path.join(__dirname,"views/index.html"));        
         });
+
+        
+        app.get("/about", (req, res)=>{
+            res.sendFile(path.join(__dirname, "/views/about.html"));
+        })
 
         // this route gets result for the getallsets function
         app.get("/lego/sets", async (req, res) => {
@@ -58,31 +57,31 @@ legoData.initialize()
             }
         });
 
-        app.get("/about", (req, res)=>{
-            res.sendFile(path.join(__dirname, "/views/about.html"));
-          })
           
          // this route gets result for the getSetByTheme function 
-        app.get("/lego/sets/:name", async (req, res) => {
-            try {
-                //getting the information of set who has icon theme of function getsetbynum 
-                const sets = await legoData.getSetsByTheme(req.params.name);
-                res.send(sets);
-            } catch (error) {
-                res.status(404).send("Error: Sets not found for the given theme.");
-            }
-        });
+        // app.get("/lego/sets/:name", async (req, res) => {
+        //     try {
+        //         //getting the information of set who has icon theme of function getsetbynum 
+        //         const sets = await legoData.getSetsByTheme(req.params.name);
+        //         res.send(sets);
+        //     } catch (error) {
+        //         res.status(404).send("Error: Sets not found for the given theme.");
+        //     }
+        // });
 
 
         app.use((req, res) => {
             res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
         });
         // Start the server
-        app.listen(PORT, () => {
-            console.log(`Server is running on port "http://localhost:${PORT}"`);
-        });
         
-    })
+        // Initialize Lego Data
+        legoData.initialize()
+        .then(() => { 
+            app.listen(PORT, () => {
+                console.log(`Server is running on port "http://localhost:${PORT}"`);
+            });
+        })
     .catch((error) => {
         console.error("Failed to initialize Lego Data:", error);
     });
